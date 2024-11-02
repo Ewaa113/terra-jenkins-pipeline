7fdda2bb2ead2b5a427f5f56abfa10552e1983de
Ewa Swiechowska <ewa@Ewas-MacBook-Pro.local>
2024-11-02 12:21:52 -0400

    Remove Amazon linux setup and update Ubuntu setup

--- 1-terraform-jenkins-server/jenkins-script.sh
+++ 1-terraform-jenkins-server/jenkins-script.sh
@@ -1,50 +1,30 @@
 #!/bin/bash
 
-# Update packages and install common tools
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    if [[ "$ID" == "amzn" ]]; then
-        # Commands for Amazon Linux
-        sudo yum update -y
-        sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-        sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-        sudo yum upgrade -y
-        sudo amazon-linux-extras install java-openjdk11 -y
-        sudo yum install jenkins -y
-        sudo systemctl enable jenkins
-        sudo systemctl start jenkins
+# install jenkins
 
-        # Install git, terraform, kubectl for Amazon Linux
-        sudo yum install git -y
-        sudo yum install -y yum-utils
-        sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-        sudo yum -y install terraform
-        sudo curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl"
-        sudo chmod +x ./kubectl
-        sudo mv ./kubectl /usr/local/bin/kubectl
+sudo apt update  
+sudo apt install -y wget
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install -y openjdk-11-jdk
+sudo apt install -y jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
 
-    elif [[ "$ID" == "ubuntu" ]]; then
-        # Commands for Ubuntu
-        sudo apt update -y
-        sudo apt install openjdk-11-jdk -y
-        curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-            /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-        echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-            https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-            /etc/apt/sources.list.d/jenkins.list > /dev/null
-        sudo apt update -y
-        sudo apt install jenkins -y
-        sudo systemctl enable jenkins
-        sudo systemctl start jenkins
+# install git
 
-        # Install git, terraform, kubectl for Ubuntu
-        sudo apt install git -y
-        sudo apt install -y software-properties-common
-        curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-        echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-        sudo apt update -y && sudo apt install terraform -y
-        curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl"
-        sudo chmod +x ./kubectl
-        sudo mv ./kubectl /usr/local/bin/kubectl
-    fi
-fi
+sudo apt install -y git
+
+# install terraform
+
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt update
+sudo apt install -y terraform
+
+# install kubectl
+
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl  
--- 1-terraform-jenkins-server/server.tf
+++ 1-terraform-jenkins-server/server.tf
@@ -1,18 +1,5 @@
-# Amazon Linux AMI data source
-data "aws_ami" "latest_amazon_linux_image" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 # Ubuntu AMI data source
+
 data "aws_ami" "latest_ubuntu_image" {
   most_recent = true
   owners      = ["099720109477"]  # Canonical's AWS account ID for Ubuntu AMIs
@@ -26,22 +13,8 @@ data "aws_ami" "latest_ubuntu_image" {
   }
 }
 
-# Original Amazon Linux instance
-resource "aws_instance" "my-server" {
-  ami                         = data.aws_ami.latest_amazon_linux_image.id
-  instance_type               = var.instance_type
-  key_name                    = "jenkins-server-demo"
-  subnet_id                   = aws_subnet.jenkins-subnet-1.id
-  vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
-  availability_zone           = var.availability_zone
-  associate_public_ip_address = true
-  user_data                   = file("jenkins-script.sh")
-  tags = {
-    Name = "${var.env_prefix}-old-server"
-  }
-}
-
 # New Ubuntu instance
+
 resource "aws_instance" "my-server-new" {
   ami                         = data.aws_ami.latest_ubuntu_image.id
   instance_type               = var.instance_type
